67f9fe4f714c25c8c1813ead77e3dc48babf1f74
gchulev <georgi.chulev@gmail.com>
2022-04-01 16:11:54 +0300

    Added SoftUni problem 'Reverse Strings'

---
+++ CSharpAdvanced/Reverse Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Stack<string> stack = new Stack<string>();
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i].ToString());
+            }
+            foreach (string item in stack)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
