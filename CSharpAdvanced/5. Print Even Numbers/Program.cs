0cade4af62b2dbff50da57d8e386ef80d5a81b41
gchulev <georgi.chulev@gmail.com>
2022-04-05 11:37:52 +0300

    Added SoftUni problem 'Print Even Numbers'

--- CSharpAdvanced/5. Print Even Numbers/Program.cs
+++ CSharpAdvanced/5. Print Even Numbers/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _5._Print_Even_Numbers
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(input);
+            List<int> evenNumbers = new List<int>();
+
+            for (int i = 0; i < queue.Count; i++)
+            {
+                if (queue.Peek() % 2 == 0)
+                {
+                    evenNumbers.Add(queue.Peek());
+                    queue.Dequeue();
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+                i--;
+            }
+            Console.WriteLine(string.Join(", ", evenNumbers));
         }
     }
 }
