4e7cafd053280da20b9b0e5fe6e7d8ec1b3c99fa
gchulev <georgi.chulev@gmail.com>
2022-04-05 10:13:51 +0300

    Added SoftUni problem 'Simple Calculator'

---
+++ CSharpAdvanced/3. Simple Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Stack<string> stack = new Stack<string>(input.Reverse());
+            int sum = 0;
+            string currentSign = string.Empty;
+            for (int i = 0; i < stack.Count; i++)
+            {
+                string item = stack.Pop();
+                if (item == "-")
+                {
+                    currentSign = "-";
+                }
+                else if (item == "+")
+                {
+                    currentSign = "+";
+                }
+                else
+                {
+                    if (currentSign == "-")
+                    {
+                        sum -= int.Parse(item);
+                    }
+                    else if (currentSign == "+")
+                    {
+                        sum += int.Parse(item);
+                    }
+                    else
+                    {
+                        sum += int.Parse(item);
+                    }
+                }
+                i--;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
