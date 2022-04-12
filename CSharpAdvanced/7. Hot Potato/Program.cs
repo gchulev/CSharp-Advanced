75893a13a449e209b85894821a4b45c6f91856ae
gchulev <georgi.chulev@gmail.com>
2022-04-12 19:46:01 +0300

    Added SoftUni problem 'Hot Potato'

---
+++ CSharpAdvanced/7. Hot Potato/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Queue<string> queue = new Queue<string>(Console.ReadLine().Split(' '));
+            int toss = int.Parse(Console.ReadLine());
+            int i = 1;
+            while (queue.Count > 1)
+            {
+                string currentChild = queue.Dequeue();
+                if (i == toss)
+                {
+                    Console.WriteLine($"Removed {currentChild}");
+                    i = 1;
+                }
+                else
+                {
+                    queue.Enqueue(currentChild);
+                    i++;
+                }
+            }
+            Console.WriteLine($"Last is {string.Join(' ', queue)}");
+        }
+    }
+}
