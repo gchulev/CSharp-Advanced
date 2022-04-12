08549e927a9b5d212c2e9d8c02655ffc1c8a8593
gchulev <georgi.chulev@gmail.com>
2022-04-12 20:01:26 +0300

    Added SoftUni problem 'Traffic Jam'

---
+++ CSharpAdvanced/8. Traffic Jam/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numOfCars = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+            Queue<string> queue = new Queue<string>();
+            List<string> passedCars = new List<string>();
+
+            while (command != "end")
+            {
+                if (command.Equals("green"))
+                {
+                    for (int i = 0; i < numOfCars; i++)
+                    {
+                        if (queue.Count == 0)
+                        {
+                            continue;
+                        }
+                        string currentCar = queue.Dequeue();
+                        passedCars.Add(currentCar);
+                        Console.WriteLine($"{currentCar} passed!");
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{passedCars.Count} cars passed the crossroads.");
+        }
+    }
+}
