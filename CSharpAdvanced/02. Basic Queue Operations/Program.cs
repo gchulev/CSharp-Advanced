f98e0919a16fcfaf2efa9b577204c97fd14230e8
gchulev <georgi.chulev@gmail.com>
2022-04-13 13:29:19 +0300

    Added Softuni problems 'Basic Stack Operations' and 'Basic Queue Operations'

---
+++ CSharpAdvanced/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int numsToAdd = input[0];
+            int numsToRemove = input[1];
+            int numToCheck = input[2];
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < numsToAdd; i++)
+            {
+                queue.Enqueue(numbers[i]);
+            }
+            for (int i = 0; i < numsToRemove; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(numToCheck))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/1. Basic Stack Operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] numArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int numToPush = input[0];
+            int numToPop = input[1];
+            int numToCheck = input[2];
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < numToPush; i++)
+            {
+                stack.Push(numArray[i]);
+            }
+            for (int j = 0; j < numToPop; j++)
+            {
+                stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (stack.Contains(numToCheck))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
