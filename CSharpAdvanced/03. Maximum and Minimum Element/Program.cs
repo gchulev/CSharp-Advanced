2bca76fe1725bb3db842966d4db294276cc295ce
gchulev <georgi.chulev@gmail.com>
2022-04-13 19:19:11 +0300

    Added SoftUni problem 'Maximum and Minimum Element'

---
+++ CSharpAdvanced/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] query = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                if (query[0].Equals(1))
+                {
+                    stack.Push(query[1]);
+                }
+                else if (query[0].Equals(2))
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (query[0].Equals(3))
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else if (query[0].Equals(4))
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+            if (stack.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", stack));
+            }
+        }
+    }
+}
