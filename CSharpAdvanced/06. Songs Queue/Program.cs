d0b34d4b08d1196a384c9b04380d489396ed8d67
gchulev <georgi.chulev@gmail.com>
2022-04-14 17:55:44 +0300

    Added SoftUni problem 'Songs Queue'

---
+++ CSharpAdvanced/06. Songs Queue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new Queue<string>(Console.ReadLine().Split(", "));
+
+            while (songs.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command.Contains("Play"))
+                {
+                    songs.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string song = command.Replace("Add", "").Trim();
+                    if (!songs.Contains(song))
+                    {
+                        songs.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                }
+                else if (command.Contains("Show"))
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
