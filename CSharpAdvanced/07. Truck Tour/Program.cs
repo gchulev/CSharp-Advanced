f8b62c64d527173e3b7a3fb6dd1f1681dbe87db4
gchulev <georgi.chulev@gmail.com>
2022-05-21 10:44:56 +0300

    Added softuni problem 'Truck Tour'

---
+++ CSharpAdvanced/07. Truck Tour/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Queue<GasPump> queue = new Queue<GasPump>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                int fuel = int.Parse(input.Split()[0]);
+                int distance = int.Parse(input.Split()[1]);
+                var pump = new GasPump(i, fuel, distance);
+                queue.Enqueue(pump);
+            }
+
+            int startingPumpIndex = 0;
+            while (true)
+            {
+                int gasInTank = 0;
+                foreach (var pump in queue)
+                {
+                    int currentIndex = pump.PumpIndex;
+                    int currentFuel = pump.Fuel;
+                    int distanceToNextPump = pump.Distance;
+
+                    gasInTank += currentFuel;
+                    gasInTank -= distanceToNextPump;
+
+                    if (gasInTank < 0)
+                    {
+                        GasPump itemToMove = queue.Dequeue();
+                        queue.Enqueue(itemToMove);
+                        startingPumpIndex++;
+                        break;
+                    }
+                }
+
+                if (gasInTank >= 0)
+                {
+                    Console.WriteLine(startingPumpIndex);
+                    return;
+                }
+
+            }
+
+        }
+    }
+
+    class GasPump
+    {
+        public int PumpIndex { get; set; }
+        public int Fuel { get; set; }
+        public int Distance { get; set; }
+
+        public GasPump(int index, int fuel, int distance)
+        {
+            PumpIndex = index;
+            Fuel = fuel;
+            Distance = distance;
+        }
+    }
+}
