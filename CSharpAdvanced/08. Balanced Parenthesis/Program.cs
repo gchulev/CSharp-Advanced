e6aefaa5a059631bf2e2ea80842af7945a35960a
gchulev <georgi.chulev@gmail.com>
2022-05-22 08:07:08 +0300

    Added SoftUni problem 'Balanced Parenthesis'

---
+++ CSharpAdvanced/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<char> openedBrakets = new Stack<char>();
+            bool balance = false;
+
+            foreach (char braket in input)
+            {
+                if (braket == '{' || braket == '(' || braket == '[')
+                {
+                    openedBrakets.Push(braket);
+                }
+                else if (braket == '}' || braket == ')' || braket == ']')
+                {
+                    if (openedBrakets.Count > 0)
+                    {
+                        char lastOpenBraket = openedBrakets.Pop();
+
+                        if (lastOpenBraket == '{' && braket == '}')
+                        {
+                            balance = true;
+                        }
+                        else if (lastOpenBraket == '(' && braket == ')')
+                        {
+                            balance = true;
+                        }
+                        else if (lastOpenBraket == '[' && braket == ']')
+                        {
+                            balance = true;
+                        }
+                        else
+                        {
+                            balance = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        balance = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    balance = false;
+                    break;
+                }
+            }
+            if (balance)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
