9c56fce4b93e44b485ca735503a22f317fd7ba13
gchulev <georgi.chulev@gmail.com>
2022-05-22 14:40:30 +0300

    Added SoftUni problem 'Diagonal Difference'

--- CSharpAdvanced/1. Diagonal Difference/Program.cs
+++ CSharpAdvanced/1. Diagonal Difference/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Linq;
 
 namespace _1._Diagonal_Difference
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] lineInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = lineInput[col];
+                }
+            }
+
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    if (r == c)
+                    {
+                        primaryDiagonalSum += matrix[r, c];
+                    }
+                    var tet = matrix.GetLength(1) - r - 1;
+                }
+                secondaryDiagonalSum += matrix[r, matrix.GetLength(1) - r - 1];
+            }
+
+            int difference = Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+            Console.WriteLine($"{difference}");
         }
     }
 }
