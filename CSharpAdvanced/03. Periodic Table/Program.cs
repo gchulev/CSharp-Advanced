bc8dd35f2549d7edb211a3db16d6f3fb74ebf18b
gchulev <georgi.chulev@gmail.com>
2022-05-27 12:08:21 +0300

    Added SoftUni problem 'Periodic Table'

---
+++ CSharpAdvanced/03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var uniqueElements = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (string element in elements)
+                {
+                    uniqueElements.Add(element);
+                }
+            }
+            var orderedUniqueElements = uniqueElements.OrderBy(x => x).ToArray();
+            Console.WriteLine(String.Join(' ', orderedUniqueElements));
+        }
+    }
+}
