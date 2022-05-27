3d6cb315d2d48cdfecd0ac8a54252e12d158bc9b
gchulev <georgi.chulev@gmail.com>
2022-05-27 12:44:52 +0300

    Added SoftUni problem 'Even Times'

---
+++ CSharpAdvanced/04. Even Times/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers[num] = 0;
+                }
+                numbers[num] += 1;
+            }
+
+            int evenTimesNum = numbers.First(x => x.Value % 2 == 0).Key;
+            Console.WriteLine(evenTimesNum);
+        }
+    }
+}
