dfa7ae58435c86ed0fad23d2afdcb44abf23492b
gchulev <georgi.chulev@gmail.com>
2022-05-27 11:52:49 +0300

    Added SoftUni problem 'Unique Usernames'

---
+++ CSharpAdvanced/01. Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var users = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string userName = Console.ReadLine();
+                users.Add(userName);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, users));
+        }
+    }
+}
