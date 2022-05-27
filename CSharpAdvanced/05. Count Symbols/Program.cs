6bdb0dbcf88b233d190860990817f2a9f98e3ff9
gchulev <georgi.chulev@gmail.com>
2022-05-27 12:55:45 +0300

    Added SoftUni problem 'Count symbols'

---
+++ CSharpAdvanced/05. Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var counterDict = new Dictionary<char, int>();
+
+            foreach (char character in input)
+            {
+                if (!counterDict.ContainsKey(character))
+                {
+                    counterDict.Add(character, 0);
+                }
+                counterDict[character]++;
+            }
+            var sortedDict = counterDict.OrderBy(x => x.Key).ToDictionary(x => x.Key, y => y.Value);
+            foreach (var kvp in sortedDict)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
