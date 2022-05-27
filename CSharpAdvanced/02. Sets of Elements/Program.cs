3196a65c1030279e0e38d66fd3382a2c70d7e127
gchulev <georgi.chulev@gmail.com>
2022-05-27 12:02:19 +0300

    Added SoftUni problem 'Sets of Elements'

--- CSharpAdvanced/02. Sets of Elements/Program.cs
+++ CSharpAdvanced/02. Sets of Elements/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Sets_of_Elements
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] setsLength = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int n = setsLength[0];
+            int m = setsLength[1];
+            var set1 = new HashSet<int>();
+            var set2 = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int numToAdd = int.Parse(Console.ReadLine());
+                set1.Add(numToAdd);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                int numToAdd = int.Parse(Console.ReadLine());
+                set2.Add(numToAdd);
+            }
+
+            int[] set3 = set1.Intersect(set2).ToArray();
+            Console.WriteLine(String.Join(' ', set3));
         }
     }
 }
