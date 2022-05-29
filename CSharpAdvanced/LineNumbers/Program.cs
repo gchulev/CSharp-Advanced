8824a69cede536d59095a8e56a3a1bbe45fe8dc0
gchulev <georgi.chulev@gmail.com>
2022-05-29 11:07:46 +0300

    Added SoftUni problem 'LineNumbers'

---
+++ CSharpAdvanced/LineNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            var fileContent = File.ReadAllLines(inputFilePath);
+            int lineCounter = 1;
+            var outputList = new List<string>();
+
+            foreach (string line in fileContent)
+            {
+                int charCount = line.Count(x => char.IsLetter(x));
+                int punctuationMarksCount = line.Count(x => char.IsPunctuation(x));
+                outputList.Add($"Line {lineCounter}: {line} ({charCount})({punctuationMarksCount})");
+                lineCounter++;
+            }
+
+            File.WriteAllLines(outputFilePath, outputList);
+        }
+    }
+}
