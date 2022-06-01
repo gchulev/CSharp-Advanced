bbf77a9b0b0f8f692b82890da96185c5cf84f3f0
gchulev <georgi.chulev@gmail.com>
2022-06-01 17:25:33 +0300

    Added SoftUni problem 'Copy Binary File'

---
+++ CSharpAdvanced/CopyBinaryFile/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+
+            using (var fs = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (var output = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    while (true)
+                    {
+                        var readData = fs.Read(buffer, 0, buffer.Length);
+                        if (readData == 0)
+                        {
+                            break;
+                        }
+                        output.Write(buffer, 0, readData);
+                    }
+                }
+            }
+        }
+    }
+}
+
