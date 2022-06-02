11ee7ce52aeb006baee2d87bfc0c4a195317de1a
gchulev <georgi.chulev@gmail.com>
2022-06-02 20:36:18 +0300

    Added SoftUni problem 'CopyDirectory'

---
+++ CSharpAdvanced/CopyDirectory/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.Linq;
+    using System.IO;
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"H:\Temp";
+            string outputPath = @$"H:\Temp\Output";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            var inputDir = new DirectoryInfo(inputPath);
+            var files = inputDir.GetFiles();
+
+
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+            Directory.CreateDirectory(outputPath);
+
+            foreach (FileInfo file in files)
+            {
+                File.Copy(file.FullName, Path.Combine(outputPath, file.Name));
+            }
+        }
+    }
+}
