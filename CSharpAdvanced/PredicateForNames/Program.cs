ef42421c9cac1ef7a38bc3c8d82d8d5ab7fa9a31
gchulev <georgi.chulev@gmail.com>
2022-06-03 18:10:18 +0300

    Added SoftUni problem 'Predicate For Names'

---
+++ CSharpAdvanced/PredicateForNames/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateForNames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Predicate<string> isLongerThan = x => x.Length <= length;
+            var result = names.FindAll(x => isLongerThan(x)).ToList();
+            Console.WriteLine(String.Join($"{Environment.NewLine}", result));
+        }
+    }
+}
--- CSharpAdvanced/ReverseAndExclude/Program.cs
+++ CSharpAdvanced/ReverseAndExclude/Program.cs
@@ -11,9 +11,7 @@ namespace ReverseAndExclude
             List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
             int num = int.Parse(Console.ReadLine());
 
-            Func<int, int, bool> isDivisible = (x, y) => x % y == 0;
             numbers.Reverse();
-            var test = numbers.Select(x => isDivisible(x, num)).ToList();
             Predicate<int> isDivisbleBynNum = x => x % num == 0;
             numbers.RemoveAll(isDivisbleBynNum);
             Console.WriteLine(String.Join(' ', numbers));
