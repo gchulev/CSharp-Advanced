8c7b732f35fd155a7c57888268273dd425802278
gchulev <georgi.chulev@gmail.com>
2022-06-03 15:39:24 +0300

    Added SoftUni problem 'ActionPrint'

---
+++ CSharpAdvanced/ActionPrint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ActionPrint
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Action<string> printAction = Print;
+
+            foreach (string item in input)
+            {
+                printAction(item);
+            }
+        }
+
+        public static void Print(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
