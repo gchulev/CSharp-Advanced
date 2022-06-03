aad04ff227ec11685a507bd082e5d85533bc077c
gchulev <georgi.chulev@gmail.com>
2022-06-03 17:22:48 +0300

    Added SoftUni problem 'Applied Arithemtics'

---
+++ CSharpAdvanced/AppliedArithmetics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppliedArithmetics
+{
+    public delegate List<int> MathOperations(List<int> list, int num);
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> nums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command.Equals("end"))
+                {
+                    break;
+                }
+                else if (command.Equals("add"))
+                {
+                    MathOperations addition = (list, num) => list.Select(x => x + num).ToList();
+                    nums = addition(nums, 1);
+                }
+                else if (command.Equals("multiply"))
+                {
+                    MathOperations multiply = (list, num) => list.Select(x => x * num).ToList();
+                    nums = multiply(nums, 2);
+                }
+                else if (command.Equals("subtract"))
+                {
+                    MathOperations substract = (list, num) => list.Select(x => x - num).ToList();
+                    nums = substract(nums, 1);
+                }
+                else if (command.Equals("print"))
+                {
+                    Console.WriteLine(String.Join(' ', nums));
+                }
+            }
+        }
+    }
+}
