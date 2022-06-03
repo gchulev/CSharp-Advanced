8df5f6d84b891b77fe42948237d4a002e7aa4b76
gchulev <georgi.chulev@gmail.com>
2022-06-03 16:54:55 +0300

    Added SoftUni problem 'Find Evens or Odds'

---
+++ CSharpAdvanced/Find EvensOrOdds/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Find_EvensOrOdds
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            int lowerBound = int.Parse(input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0]);
+            int upperBound = int.Parse(input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1]);
+            string command = Console.ReadLine();
+            var numbers = new List<int>();
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> isEvenOrOdd = null;
+
+            var result = new List<int>();
+
+            if (command.Equals("even"))
+            {
+                isEvenOrOdd = num => num % 2 == 0;
+            }
+            else if (command.Equals("odd"))
+            {
+                isEvenOrOdd = num => num % 2 != 0;
+            }
+            result = numbers.FindAll(isEvenOrOdd);
+            Console.WriteLine(String.Join(' ', result));
+        }
+    }
+}
