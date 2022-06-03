28c18ba7709042e8af5afcd280a3ee9c219a2265
gchulev <georgi.chulev@gmail.com>
2022-06-03 18:01:56 +0300

    Added SoftUni problem 'Reverse And Exclude'

---
+++ CSharpAdvanced/ReverseAndExclude/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReverseAndExclude
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int num = int.Parse(Console.ReadLine());
+
+            Func<int, int, bool> isDivisible = (x, y) => x % y == 0;
+            numbers.Reverse();
+            var test = numbers.Select(x => isDivisible(x, num)).ToList();
+            Predicate<int> isDivisbleBynNum = x => x % num == 0;
+            numbers.RemoveAll(isDivisbleBynNum);
+            Console.WriteLine(String.Join(' ', numbers));
+        }
+    }
+}
