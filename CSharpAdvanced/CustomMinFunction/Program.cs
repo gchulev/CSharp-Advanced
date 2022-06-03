d065dca60c2793dede00cd377c6ece73d81869dd
gchulev <georgi.chulev@gmail.com>
2022-06-03 15:55:03 +0300

    Added SoftUni problem 'Custom Min Function'

---
+++ CSharpAdvanced/CustomMinFunction/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomMinFunction
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Func<int[], int> result = n => n.Min();
+
+            Console.WriteLine(result(input)); 
+        }
+    }
+}
