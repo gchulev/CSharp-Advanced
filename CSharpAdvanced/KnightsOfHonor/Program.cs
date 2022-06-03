3beeebfa77cd0bfb7ac89f1084a0d8abf162509f
gchulev <georgi.chulev@gmail.com>
2022-06-03 15:43:56 +0300

    Added SoftUni problem 'Knights of Honor'

---
+++ CSharpAdvanced/KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KnightsOfHonor
+{
+    internal class KnightsOfHonor
+    {
+        static void Main()
+        {
+            string[] collection = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> action = x => Console.WriteLine($"Sir {x}");
+
+            foreach (string item in collection)
+            {
+                action(item);
+            }
+        }
+    }
+}
