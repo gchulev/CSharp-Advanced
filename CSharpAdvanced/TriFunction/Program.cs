6e12844290d33baeb50e392315ad9ac6a9043a7f
gchulev <georgi.chulev@gmail.com>
2022-06-04 18:40:17 +0300

    Added SoftUni problem 'Trifunction'

---
+++ CSharpAdvanced/TriFunction/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TriFunction
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+
+            Func<string, int, bool> charactersSumLongerThanN = (inputString, inputNumber) =>
+                    {
+                        int charSum = 0;
+                        for (int i = 0; i < inputString.Length; i++)
+                        {
+                            charSum += inputString[i];
+                        }
+                        if (charSum >= inputNumber)
+                        {
+                            return true;
+                        }
+                        return false;
+                    };
+
+            string nameFound = FindFirstEqualOrLongerName(input, charactersSumLongerThanN);
+            Console.WriteLine(nameFound);
+
+            string FindFirstEqualOrLongerName(List<string> inputList, Func<string, int, bool> func)
+            {
+                string output = string.Empty;
+                foreach (string name in inputList)
+                {
+                    if (func(name, n))
+                    {
+                        output = name;
+                        return output;
+                    }
+                }
+                return output;
+            }
+        }
+    }
+}
