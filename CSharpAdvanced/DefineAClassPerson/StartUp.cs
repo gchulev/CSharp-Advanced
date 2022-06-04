c92df4cd8d11baeb2cc1407b7f0e1a9ab6098b8a
gchulev <georgi.chulev@gmail.com>
2022-06-04 19:31:28 +0300

    Added SoftUni problem 'DefineAClassPerson'

---
+++ CSharpAdvanced/DefineAClassPerson/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DefiningClasses
+
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string name = "Peter";
+            int age = 45;
+            Person person = new Person(name, age);
+            Person anotherPerson = new Person();
+            Person thirdPerson = new Person("Michael", 24);
+            Person fourthPerson = new Person()
+            {
+                Name = "Josh",
+                Age = 70
+            };
+        }
+    }
+}
