7ab3ac6ce05bb63adefa4eeff4666311fc196bf8
gchulev <georgi.chulev@gmail.com>
2022-06-04 14:40:45 +0300

    Added SoftUni problems 'List Of Predicates' and 'Predicate Party'

---
+++ CSharpAdvanced/ListOfPredicates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfPredicates
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<int> uniqueNums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToHashSet();
+
+            List<int> numbers = uniqueNums.ToList();
+
+            Func<List<int>, List<int>> dividers =
+                    ls =>
+                    {
+                        var output = new List<int>();
+
+                        for (int i = 1; i <= n; i++)
+                        {
+                            bool canBeDivided = true;
+                            for (int c = 0; c < ls.Count; c++)
+                            {
+                                if (i % ls[c] != 0)
+                                {
+                                    canBeDivided = false;
+                                    break;
+                                }
+                            }
+                            if (canBeDivided)
+                            {
+                                output.Add(i);
+                            }
+                        }
+                        return output;
+                    };
+            Console.WriteLine($"{string.Join(' ', dividers(numbers))}");
+        }
+    }
+}
---
+++ CSharpAdvanced/PredicateParty!/PredicateParty.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace PredicateParty_
+{
+    internal class PredicateParty
+    {
+        static void Main()
+        {
+            List<string> people = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            while (true)
+            {
+                string inputCommand = Console.ReadLine();
+                string command = inputCommand.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0];
+
+                if (command.Equals("Party!"))
+                {
+                    break;
+                }
+
+                Predicate<string> filter = str =>
+                {
+                    string cmd = inputCommand.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
+                    string argument = inputCommand.Split(' ', StringSplitOptions.RemoveEmptyEntries)[2];
+
+                    if (cmd.Equals("StartsWith"))
+                    {
+                        if (!str.StartsWith(argument))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
+                    else if (cmd.Equals("EndsWith"))
+                    {
+                        if (!str.EndsWith(argument))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
+                    else if (cmd.Equals("Length"))
+                    {
+                        if (!(str.Length == int.Parse(argument)))
+                        {
+                            return false;
+                        }
+                        return true;
+                    }
+                    return false;
+                };
+
+                if (command.Equals("Remove"))
+                {
+                    people.RemoveAll(filter);
+                }
+                else if (command.Equals("Double"))
+                {
+                    for (int i = 0; i < people.Count; i++)
+                    {
+                        string person = people[i];
+                        if (filter(person))
+                        {
+                            people.Insert(i + 1, person);
+                            i++;
+                        }
+                    }
+                }
+            }
+            if (people.Count > 0)
+            {
+                Console.WriteLine($"{string.Join(", ", people)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+    }
+}
