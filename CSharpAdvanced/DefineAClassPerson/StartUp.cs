25fa09310d5de8315552d5aae45872df683fd594
gchulev <georgi.chulev@gmail.com>
2022-06-05 20:48:23 +0300

    Added changes to SoftUni problem 'Defning Classes', added task 4 - 4. Opinion Poll

--- CSharpAdvanced/DefineAClassPerson/StartUp.cs
+++ CSharpAdvanced/DefineAClassPerson/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DefiningClasses
 
@@ -8,19 +10,29 @@ namespace DefiningClasses
         static void Main()
         {
             int n = int.Parse(Console.ReadLine());
-            Family family = new Family();
+            List<Person> people = new List<Person>();
+
+            //Family family = new Family();
 
             for (int i = 0; i < n; i++)
             {
                 string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
                 string name = input[0];
                 int age = int.Parse(input[1]);
-
                 var currentPerson = new Person(name, age);
-                family.AddMember(currentPerson);
+                people.Add(currentPerson);
+                //family.AddMember(currentPerson);
             }
 
-            Console.WriteLine($"{family.GetOldestMember().Name} {family.GetOldestMember().Age}"); 
+            //Console.WriteLine($"{family.GetOldestMember().Name} {family.GetOldestMember().Age}"); 
+
+            var output = people.Where(x => x.Age > 30).OrderBy(y => y.Name).ToList();
+
+            foreach (Person p in output)
+            {
+                Console.WriteLine($"{p.Name} - {p.Age}");
+            }
+
         }
     }
 }
