5617e3ce6a0bb64f5d7dfc1958ff554b33c58896
gchulev <georgi.chulev@gmail.com>
2022-06-06 10:11:54 +0300

    Added SoftUni problem 'Speed Racing'

---
+++ CSharpAdvanced/SpeedRacing/Car.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string _model;
+        private double _fuelAmount;
+        private double _fuelConsumptionPerKilometer;
+        private double _travelledDistance;
+
+        public string Model
+        {
+            get
+            {
+                return _model;
+            }
+            set { _model = value; }
+        }
+        public double FuelAmount
+        {
+            get
+            {
+                return _fuelAmount;
+            }
+            set
+            {
+                _fuelAmount = value;
+            }
+        }
+        public double FuelconsumptionPerKilometer
+        {
+            get
+            {
+                return _fuelConsumptionPerKilometer;
+            }
+            set
+            {
+                _fuelConsumptionPerKilometer = value;
+            }
+        }
+        public double TravelledDistance 
+        {
+            get 
+            {
+                return _travelledDistance;
+            }
+            set
+            {
+                _travelledDistance = value;
+            } 
+        }
+
+        public Car()
+        {
+            TravelledDistance = 0;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumption)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelconsumptionPerKilometer = fuelConsumption;
+        }
+
+        public void MoveCar(double kmTravelled)
+        {
+            double fuelNeeded = kmTravelled * FuelconsumptionPerKilometer;
+            if (FuelAmount >= fuelNeeded)
+            {
+                FuelAmount -= fuelNeeded;
+                TravelledDistance += kmTravelled;
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/SpeedRacing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string carModel = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelConsumption = double.Parse(input[2]);
+
+                cars.Add(new Car(carModel, fuelAmount, fuelConsumption));
+            }
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string command = input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0];
+
+                if (input.Equals("End"))
+                {
+                    break;
+                }
+                else if (command.Equals("Drive"))
+                {
+                    string carModel = input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1];
+                    double traveledDistance = double.Parse(input.Split(' ', StringSplitOptions.RemoveEmptyEntries)[2]);
+                    var currentCar = cars.Find(x => x.Model == carModel);
+                    currentCar.MoveCar(traveledDistance);
+                }
+            }
+
+            foreach (Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
