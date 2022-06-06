39cf81a61eae90688137405fa59a184eaa4328a5
gchulev <georgi.chulev@gmail.com>
2022-06-06 07:45:51 +0300

    Added SoftUni problem 'DateModifier'

---
+++ CSharpAdvanced/DateModifier/DateModifier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        private DateTime _firstDate;
+        private DateTime _secondDate;
+
+        public DateTime FirstDate { get; set; }
+        public DateTime SecondDate { get; set; }
+
+        public DateModifier(string date1, string date2)
+        {
+            FirstDate = DateTime.Parse(date1);
+            SecondDate = DateTime.Parse(date2);
+        }
+
+        public void CalculateDateDifference(string date1, string date2)
+        {
+            DateModifier modifier = new DateModifier(date1, date2);
+            double dateDiff = Math.Abs(modifier.FirstDate.Subtract(modifier.SecondDate).TotalDays);
+            Console.WriteLine(dateDiff);
+        }
+    }
+}
---
+++ CSharpAdvanced/DateModifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateModifier
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            string date1 = Console.ReadLine();
+            string date2 = Console.ReadLine();
+            DateModifier modifier = new DateModifier(date1, date2);
+
+            modifier.CalculateDateDifference(date1, date2);
+
+
+        }
+    }
+}
