b033c90fb15d32fc8fd2a0df4254ba98cec7a65b
gchulev <georgi.chulev@gmail.com>
2022-06-09 16:34:52 +0300

    Added SoftUni problem 'Generic Box'

---
+++ CSharpAdvanced/GenericBoxOfString/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T _data;
+        public Box(T data)
+        {
+            _data = data;
+        }
+
+        public override string ToString()
+        {
+            return $"{_data.GetType()}: {_data}";
+        }
+    }
+}
---
+++ CSharpAdvanced/GenericBoxOfString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                var box = new Box<int>(input);
+                Console.WriteLine(box.ToString());
+            }
+        }
+    }
+}
