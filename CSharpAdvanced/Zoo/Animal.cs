d0e4d4631c80f4f62459213d450f5c1b110c8c3e
gchulev <georgi.chulev@gmail.com>
2022-06-09 16:00:50 +0300

    Added SoftUni problem 'Zoo'

---
+++ CSharpAdvanced/Zoo/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        public string Species { get; set; }
+        public string Diet { get; set; }
+        public double Weight { get; set; }
+        public double Length { get; set; }
+
+        public Animal(string species, string diet, double weight, double length)
+        {
+            Species = species;
+            Diet = diet;
+            Weight = weight;
+            Length = length;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"The {this.Species} is a {this.Diet} and weighs {this.Weight} kg.");
+            return sb.ToString();
+        }
+    }
+}
---
+++ CSharpAdvanced/Zoo/Zoo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+        public List<Animal> Animals;
+        public string Name;
+        private readonly int Capacity;
+
+        public Zoo(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Animals = new List<Animal>();
+        }
+
+        public string AddAnimal(Animal animal)
+        {
+            if (this.Capacity <= this.Animals.Count)
+            {
+                return "The zoo is full.";
+            }
+            if (string.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+            if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+            this.Animals.Add(animal);
+            return $"Successfully added {animal.Species} to the zoo.";
+        }
+
+        public int RemoveAnimals(string species)
+        {
+            int removedAnimalsCount = this.Animals.RemoveAll(a => a.Species == species);
+            return removedAnimalsCount;
+        }
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            var animalsByDiet = this.Animals.FindAll(animal => animal.Diet == diet);
+            return animalsByDiet;
+        }
+
+        public Animal GetAnimalByWeight(double weight)
+        {
+            var animal = this.Animals.FirstOrDefault(animal => animal.Weight == weight);
+            return animal;
+        }
+
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            int animalsCount = this.Animals.Where(animal => animal.Length <= maximumLength && animal.Length >= minimumLength).Count();
+            return $"There are {animalsCount} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+    }
+}
