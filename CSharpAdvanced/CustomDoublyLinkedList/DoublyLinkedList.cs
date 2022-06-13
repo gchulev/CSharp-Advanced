cc3b64b624ec0ce8fe6875bc59fb8c923a3901d3
gchulev <georgi.chulev@gmail.com>
2022-06-13 17:41:20 +0300

    Added SoftUni problem 'Custom DoublyLinkedList'

--- CSharpAdvanced/CustomDoublyLinkedList/DoublyLinkedList.cs
+++ CSharpAdvanced/CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -6,7 +6,109 @@ namespace CustomDoublyLinkedList
 {
     public class DoublyLinkedList
     {
-        private class ListNode
+        public int Count { get; private set; }
+        public ListNode Head { get; private set; }
+        public ListNode Tail { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (this.Count == 0)
+            {
+                this.Head = this.Tail = new ListNode(element);
+            }
+            else
+            {
+                var newHead = new ListNode(element);
+                newHead.NextNode = this.Head;
+                this.Head.PreviousNode = newHead;
+                this.Head = newHead;
+            }
+            this.Count++;
+        }
+
+        public void AddLast(int element)
+        {
+
+            if (this.Count == 0)
+            {
+                this.Tail = this.Head = new ListNode(element);
+            }
+            else
+            {
+                var previousTail = this.Tail;
+                var newTail = new ListNode(element);
+                newTail.PreviousNode = this.Tail;
+                this.Tail.PreviousNode = newTail;
+                this.Tail = newTail;
+            }
+            this.Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var firstElement = this.Head.Value;
+            this.Head = this.Head.NextNode;
+
+            if (Head != null)
+            {
+                this.Head.PreviousNode = null;
+            }
+            else
+            {
+                this.Tail = null;
+            }
+            Count--;
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var lastElement = this.Tail.Value;
+
+            if (this.Tail.PreviousNode == null)
+            {
+                this.Tail = this.Head = null;
+            }
+            else
+            {
+                this.Tail.NextNode = null;
+                this.Tail.PreviousNode = null;
+
+                this.Tail = this.Tail.PreviousNode;
+            }
+            Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currentNode = this.Head;
+
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public List<int> ToList(int num)
+        {
+            List<int> list = new List<int>();
+            this.ForEach(n => list.Add(n));
+            return list;
+        }
+
+        public class ListNode
         {
             public int Value { get; set; }
             public ListNode NextNode { get; set; }
@@ -15,66 +117,6 @@ namespace CustomDoublyLinkedList
             public ListNode(int value)
             {
                 this.Value = value;
-            }
-
-            private ListNode Head { get; set; }
-            private ListNode Tail { get; set; }
-
-            public int Count { get; set; }
-
-            public void AddFirst(int element)
-            {
-                if (this.Count == 0)
-                {
-                    this.Head = this.Tail = new ListNode(element);
-                }
-                else
-                {
-                    var newHead = new ListNode(element);
-                    newHead.NextNode = this.Head;
-                    this.Head.PreviousNode = newHead;
-                    this.Head = newHead;
-                }
-                this.Count++;
-            }
-
-            public void AddLast(int element)
-            {
-
-                if (this.Count == 0)
-                {
-                    this.Tail = this.Head = new ListNode(element);
-                }
-                else
-                {
-                    var newTail = new ListNode(element);
-                    newTail.NextNode = this.Tail;
-                    this.Tail.PreviousNode = newTail;
-                    this.Tail = newTail;
-                }
-                Count++;
-            }
-
-            public int RemoveFirst()
-            {
-                if (this.Count == 0)
-                {
-                    throw new InvalidOperationException("The list is empty");
-                }
-
-                var firstElement = this.Head.Value;
-                this.Head = this.Head.NextNode;
-
-                if (Head != null)
-                {
-                    this.Head.PreviousNode = null;
-                }
-                else
-                {
-                    this.Tail = null;
-                }
-                Count--;
-                return firstElement;
             }
         }
     }
--- CSharpAdvanced/CustomDoublyLinkedList/StartUp.cs
+++ CSharpAdvanced/CustomDoublyLinkedList/StartUp.cs
@@ -6,7 +6,18 @@ namespace CustomDoublyLinkedList
     {
         static void Main()
         {
+            DoublyLinkedList linkedList = new DoublyLinkedList();
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(3);
+            linkedList.AddLast(4);
 
+
+            Console.WriteLine($"Head -> {linkedList.Head.Value}");
+            Console.WriteLine($"Tail -> {linkedList.Tail.Value}");
+
+            Console.WriteLine($"Head NextNode: {linkedList.Head.NextNode.Value}");
+            Console.WriteLine($"Tail PreviousNode: {linkedList.Tail.PreviousNode.Value}");
         }
     }
 }
