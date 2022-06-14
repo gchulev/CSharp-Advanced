0ee0533def8e28bfc3dd4bd42aa6e82916dbceb0
gchulev <georgi.chulev@gmail.com>
2022-06-14 18:26:10 +0300

    Added SoftUni problem 'Custom Stack'

---
+++ CSharpAdvanced/Stack/CustomStack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Collections;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> Items { get; set; }
+        public int Count { get { return this.Items.Count; } }
+
+        public CustomStack(params T[] args)
+        {
+            this.Items = new List<T>(args);
+        }
+
+        public void Push(T element)
+        {
+            this.Items.Add(element);
+        }
+
+        public T Pop()
+        {
+            var lastElement = this.Items.Last();
+            this.Items.Remove(lastElement);
+            return lastElement;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.Items.Count - 1; i >= 0; i--)
+            {
+                yield return this.Items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = this.Items.Count; i >= 0; i--)
+            {
+                yield return this.Items[i];
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced/Stack/StartUp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var stack = new CustomStack<string>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                string command = input[0];
+
+                if (command.Equals("END"))
+                {
+                    break;
+                }
+
+                if (command.Equals("Push"))
+                {
+                    if (input.Length > 1)
+                    {
+                        string[] elements = input.Select(elm => elm.Replace(",", "")).Where(c => !string.IsNullOrWhiteSpace(c)).ToArray();
+                        for (int i = 1; i < elements.Length; i++)
+                        {
+                            stack.Push(elements[i]);
+                        }
+                    }
+                }
+                else if (command.Equals("Pop"))
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No elements");
+                    }
+                }
+
+            }
+
+            if (stack.Count > 0)
+            {
+                foreach (string item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+                foreach (string item in stack)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
