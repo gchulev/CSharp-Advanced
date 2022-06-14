387169d3463ba09d1ea17d00ca78b1aae37c3e07
gchulev <georgi.chulev@gmail.com>
2022-06-14 14:15:52 +0300

    Added SoftUni problem 'ListyIterator'

---
+++ CSharpAdvanced/ListyIterator/ListyIterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        public List<T> Items { get; private set; }
+        private int InternalIndex { get; set; }
+
+        public ListyIterator(List<T> items)
+        {
+            this.Items = items;
+            this.InternalIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (this.HasNext())
+            {
+                InternalIndex++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            int nextIndex = InternalIndex + 1;
+            if (nextIndex >= this.Items.Count)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Print()
+        {
+            if (this.Items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine($"{this.Items[InternalIndex]}");
+        }
+    }
+}
---
+++ CSharpAdvanced/ListyIterator/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var listIterator = new ListyIterator<string>(new List<string>());
+
+            while (true)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+                //string[] input = Console.ReadLine().Split();
+                string command = input[0];
+
+                if (command.Equals("END"))
+                {
+                    break;
+                }
+
+                if (command.Equals("Create"))
+                {
+                    if (input.Count > 1)
+                    {
+                        var outputList = input.Where(elm => input.IndexOf(elm) > 0);
+                        foreach (string element in outputList)
+                        {
+                            listIterator.Items.Add(element);
+                        }
+                    }
+
+                }
+                else if (command.Equals("Move"))
+                {
+                    Console.WriteLine(listIterator.Move());
+                }
+                else if (command.Equals("Print"))
+                {
+                    listIterator.Print();
+                }
+                else if (command.Equals("HasNext"))
+                {
+                    Console.WriteLine(listIterator.HasNext());
+                }
+            }
+
+        }
+    }
+}
