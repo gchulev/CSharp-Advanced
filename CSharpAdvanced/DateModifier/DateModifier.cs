33d42405e5a332b195bd19a2044bee623d066a11
gchulev <georgi.chulev@gmail.com>
2022-06-15 12:06:43 +0300

    Updated VS and had to fix some project errors

--- CSharpAdvanced/DateModifier/DateModifier.cs
+++ CSharpAdvanced/DateModifier/DateModifier.cs
@@ -8,16 +8,13 @@ namespace DateModifier
 {
     public class DateModifier
     {
-        private DateTime _firstDate;
-        private DateTime _secondDate;
-
         public DateTime FirstDate { get; set; }
         public DateTime SecondDate { get; set; }
 
         public DateModifier(string date1, string date2)
         {
-            FirstDate = DateTime.Parse(date1);
-            SecondDate = DateTime.Parse(date2);
+            this.FirstDate = DateTime.Parse(date1);
+            this.SecondDate = DateTime.Parse(date2);
         }
 
         public void CalculateDateDifference(string date1, string date2)
