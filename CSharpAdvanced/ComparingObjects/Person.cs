61723a563bfcf1f7c2af6e248e27da9600cdad4d
gchulev <georgi.chulev@gmail.com>
2022-06-15 11:58:57 +0300

    Added SoftUni problem 'Comparing Objects'

---
+++ CSharpAdvanced/ComparingObjects/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            int result = this.Name.CompareTo(other.Name);
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+                if (result == 0)
+                {
+                    result = this.Town.CompareTo(other.Town);
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ CSharpAdvanced/ComparingObjects/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Person> people = new List<Person>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+                if (input[0].Equals("END"))
+                {
+                    break;
+                }
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                string town = input[2];
+                people.Add(new Person(name, age, town));
+            }
+
+            int indexOfPersonToComapre = int.Parse(Console.ReadLine());
+            var personTocompare = people[indexOfPersonToComapre - 1];
+            int matchesFound = 0;
+            int notEqual = 0;
+
+            foreach (Person p in people)
+            {
+                if (p.CompareTo(personTocompare) == 0)
+                {
+                    matchesFound++;
+                }
+                else
+                {
+                    notEqual++;
+                }
+            }
+            if (matchesFound > 1)
+            {
+                Console.WriteLine($"{matchesFound} {notEqual} {people.Count}");
+                return;
+            }
+            Console.WriteLine("No matches");
+        }
+    }
+}
