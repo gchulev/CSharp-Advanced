2ae76f0da8b5403ac75e78ce6a71279bce82c702
gchulev <georgi.chulev@gmail.com>
2022-06-19 17:21:54 +0300

    Added SoftUni problem 'Fishing Net'

---
+++ CSharpAdvanced/FishingNet/Fish.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FishingNet
+{
+    public class Fish : IComparable<Fish>
+    {
+        public string FishType { get; set; }
+        public double Length { get; set; }
+        public double Weight { get; set; }
+
+        public Fish(string fishType, double length, double weight)
+        {
+            FishType = fishType;
+            Length = length;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"There is a {FishType}, {Length} cm. long, and {Weight} gr. in weight.";
+        }
+
+        public int CompareTo([AllowNull] Fish otherFish)
+        {
+            if (otherFish != null)
+            {
+                return -1;
+            }
+            int result = this.Length.CompareTo(otherFish.Length);
+            return result;
+        }
+    }
+}
---
+++ CSharpAdvanced/FishingNet/Net.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace FishingNet
+{
+    public class Net
+    {
+        public List<Fish> Fish { get; set; }
+        public string Material { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count
+        {
+            get
+            {
+                return Fish.Count;
+            }
+        }
+
+        public Net(string material, int capacity)
+        {
+            this.Material = material;
+            this.Capacity = capacity;
+            this.Fish = new List<Fish>();
+        }
+
+        public string AddFish(Fish fish)
+        {
+            if (this.Count >= this.Capacity)
+            {
+                return "Fishing net is full.";
+            }
+            if (string.IsNullOrWhiteSpace(fish.FishType) || fish.Length <= 0 || fish.Weight <= 0)
+            {
+                return "Invalid fish.";
+            }
+            Fish.Add(fish);
+            return $"Successfully added {fish.FishType} to the fishing net.";
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+            if (this.Fish.Exists(fish => fish.Weight.Equals(weight)))
+            {
+                Fish fishToRemove = this.Fish.Find(fish => fish.Weight.Equals(weight));
+                this.Fish.Remove(fishToRemove);
+                return true;
+            }
+            return false;
+        }
+
+        public Fish GetFish(string fishType)
+        {
+            Fish fishFound = this.Fish.Find(x => x.FishType.Equals(fishType));
+            return fishFound;
+        }
+
+        public Fish GetBiggestFish()
+        {
+            Fish longestFish = this.Fish.OrderByDescending(x => x.Length).Max();
+            return longestFish;
+        }
+
+        public string Report()
+        {
+            return $"Into the: {Material}: {Environment.NewLine}{string.Join(Environment.NewLine, this.Fish.OrderByDescending(x => x.Length))}";
+        }
+    }
+}
