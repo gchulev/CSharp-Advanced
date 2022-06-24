e63d3023bb1b6df7516ef0d2cc7661afeda16828
gchulev <georgi.chulev@gmail.com>
2022-06-24 18:13:41 +0300

    Added SoftUni Exam 'Cocktail Party'

---
+++ CSharpAdvanced/CocktailParty/Cocktail.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        public List<Ingredient> Ingredients { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+        public int CurrentAlcoholLevel { get { return Ingredients.Sum(x => x.Alcohol); } }
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+            Ingredients = new List<Ingredient>();
+        }
+
+        public void Add(Ingredient ingredient)
+        {
+            if (Capacity > Ingredients.Count && (MaxAlcoholLevel - Ingredients.Sum(i => i.Alcohol) >= ingredient.Alcohol) && !Ingredients.Any(x => x.Name == ingredient.Name))
+            {
+                Ingredients.Add(ingredient);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            return Ingredients.Remove(Ingredients.FirstOrDefault(x => x.Name.Equals(name)));
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            return Ingredients.FirstOrDefault(x => x.Name.Equals(name));
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return Ingredients.OrderByDescending(x => x.Alcohol).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            return $"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}{Environment.NewLine}{string.Join(Environment.NewLine, Ingredients)}";
+        }
+    }
+}
---
+++ CSharpAdvanced/CocktailParty/Ingredient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CocktailParty
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public int Alcohol { get; set; }
+        public int Quantity { get; set; }
+
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            Name = name;
+            Alcohol = alcohol;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"Ingredient: {Name}{Environment.NewLine}Quantity: {Quantity}{Environment.NewLine}Alcohol: {Alcohol}";
+        }
+    }
+}
