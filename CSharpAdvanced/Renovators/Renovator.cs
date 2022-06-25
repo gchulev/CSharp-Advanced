2cf9f3047e018562b2ccd0ef1da74d60602f3455
gchulev <georgi.chulev@gmail.com>
2022-06-25 09:28:58 +0300

    Added SoftUni Exam 'Renovators'

---
+++ CSharpAdvanced/Renovators/Renovator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Renovator
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Rate { get; set; }
+        public int Days { get; set; }
+        public bool Hired { get; set; } = false;
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            Name = name;
+            Type = type;
+            Rate = rate;
+            Days = days;
+        }
+
+        public override string ToString()
+        {
+            return $"-Renovator: {Name}{Environment.NewLine}--Specialty: {Type}{Environment.NewLine}--Rate per day: {Rate} BGN";
+        }
+    }
+}
